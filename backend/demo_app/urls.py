42dbe1b4ab6d86e1271e0ff7465acf9a2e9ce8a1
Antonio Vilchez <antoniovmonge@gmail.com>
2024-04-12 16:41:57 +0200

    compare fbv vs cbv

--- backend/demo_app/urls.py
+++ backend/demo_app/urls.py
@@ -4,7 +4,9 @@ from demo_app import views
 urlpatterns = [
     path("health-check", views.health_check),
     path("health-check-drf", views.health_check_drf),
+    path("hello-world/", views.hello_world),
     path("demo-version/", views.demo_version),
     path("custom-version/", views.DemoView.as_view()),
-    path("another-custom-version/", views.AnotherView.as_view())
+    path("another-custom-version/", views.AnotherView.as_view()),
+    path("apiview-class/", views.DemoAPIView.as_view()),
 ]
--- backend/demo_app/views.py
+++ backend/demo_app/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 
 from demo_app import custom_versions
 
+
 # Normal view
 def health_check(request, *args, **kwargs):
     return HttpResponse("Healthy | Demo App")
@@ -18,10 +19,32 @@ def health_check_drf(request, *args, **kwargs):
     return Response(data={"msg": "Healthy | DRF"})
 
 
+# Comparing function based views vs Class based views
+@api_view(["GET", "POST", "PUT"])
+def hello_world(request, *args, **kwargs):
+    if request.method == "POST":
+        return Response(data={"msg": "POST response block"})
+    elif request.method == "PUT":
+        return Response(data={"msg": "PUT response block"})
+    return Response(data={"msg": "GET response block"})
+
+
+class DemoAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response(data={"msg": "get request block"})
+
+    def post(self, request, *args, **kwargs):
+        return Response(data={"msg": "post request block"})
+
+    def delete(self, request, *args, **kwargs):
+        return Response(data={"msg": "delete request block"})
+
+
 @api_view(["GET"])
 def demo_version(request, *args, **kwargs):
     version = request.version
     return Response(data={"msg": f"You have hit {version} of demo-api"})
+
 
 class DemoView(APIView):
     versioning_class = custom_versions.DemoViewVersion
@@ -29,6 +52,7 @@ class DemoView(APIView):
     def get(self, request, *args, **kwargs):
         version = request.version
         return Response(data={"msg": f"You have hit {version}"})
+
 
 class AnotherView(APIView):
     versioning_class = custom_versions.AnotherViewVersion
