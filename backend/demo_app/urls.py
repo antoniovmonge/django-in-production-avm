921d499f8417142bf525730c723ca08f7bbfdb39
Antonio Vilchez <antoniovmonge@gmail.com>
2024-04-12 16:24:17 +0200

    add custom versioning

--- backend/demo_app/urls.py
+++ backend/demo_app/urls.py
@@ -4,5 +4,7 @@ from demo_app import views
 urlpatterns = [
     path("health-check", views.health_check),
     path("health-check-drf", views.health_check_drf),
-    path("demo-version/", views.demo_version)
+    path("demo-version/", views.demo_version),
+    path("custom-version/", views.DemoView.as_view()),
+    path("another-custom-version/", views.AnotherView.as_view())
 ]
--- backend/demo_app/views.py
+++ backend/demo_app/views.py
@@ -3,13 +3,16 @@ from django.http import HttpResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from demo_app import custom_versions
 
-# Create your views here.
+# Normal view
 def health_check(request, *args, **kwargs):
     return HttpResponse("Healthy | Demo App")
 
 
+# API VIEWS
 @api_view(["GET"])
 def health_check_drf(request, *args, **kwargs):
     return Response(data={"msg": "Healthy | DRF"})
@@ -19,3 +22,22 @@ def health_check_drf(request, *args, **kwargs):
 def demo_version(request, *args, **kwargs):
     version = request.version
     return Response(data={"msg": f"You have hit {version} of demo-api"})
+
+class DemoView(APIView):
+    versioning_class = custom_versions.DemoViewVersion
+
+    def get(self, request, *args, **kwargs):
+        version = request.version
+        return Response(data={"msg": f"You have hit {version}"})
+
+class AnotherView(APIView):
+    versioning_class = custom_versions.AnotherViewVersion
+
+    def get(self, request, *args, **kwargs):
+        version = request.version
+        if version == "v1":
+            # perform v1 related tasks
+            return Response(data={"msg": "v1 logic"})
+        elif version == "v2":
+            # perform v2 related tasks
+            return Response(data={"msg": "v2 logic"})
